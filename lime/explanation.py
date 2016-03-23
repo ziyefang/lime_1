33f3353abb1f435a69ab3bb7c538cd8a3efb739f
Marco Tulio Ribeiro <marcotcr@gmail.com>
2016-03-22 21:51:20 -0700

    Big changes, made explainer work on raw strings instead of vectorized data.

--- lime/explanation.py
+++ lime/explanation.py
@@ -6,8 +6,8 @@ import os.path
 import json
 import numpy as np
 import re
-import cgi
 import string
+import itertools
 
 def id_generator(size=15):
     """Helper function to generate random div ids. This is useful for embedding
@@ -17,15 +17,14 @@ def id_generator(size=15):
 
 class Explanation(object):
     """Object returned by explainers."""
-    def __init__(self, vocabulary=None, class_names=None):
+    def __init__(self, indexed_string, class_names=None):
         """Initializer.
 
         Args:
-            vocabulary: map from word to feature id.
+            indexed_string: lime_text.IndexedString, original string
             class_names: list of class names
         """
-        self.neighborhood_explanations = {}
-        self.vocabulary = vocabulary
+        self.indexed_string = indexed_string
         self.class_names = class_names
         self.local_exp = {}
         self.top_labels = None
@@ -35,20 +34,26 @@ class Explanation(object):
         if self.top_labels:
             return self.top_labels
         return self.local_exp.keys()
-    def as_list(self, label=1):
+    def as_list(self, label=1, positions=False):
         """Returns the explanation as a list.
 
         Args:
             label: desired label. If you ask for a label for which an
-                   explanation wasn't computed, will throw an exception.
+                explanation wasn't computed, will throw an exception.
+            positions: if True, also return word positions
 
         Returns:
-            list of tuples (feature_id, weight), or (word, weight) if vocabulary
-            is defined.
+            list of tuples (word, weight), or (word, positions, weight) if
+            positions=True. Word is a string, positions is a numpy array and
+            weight is a float.
         """
         exp = self.local_exp[label]
-        if self.vocabulary is not None:
-            exp = [(self.vocabulary[x[0]], x[1]) for x in exp]
+        if positions:
+            exp = [(self.indexed_string.word(x[0]),
+                    self.indexed_string.string_position(x[0]),
+                    x[1]) for x in exp]
+        else:
+            exp = [(self.indexed_string.word(x[0]), x[1]) for x in exp]
         return exp
     def as_map(self):
         """Returns the map of explanations.
@@ -84,27 +89,29 @@ class Explanation(object):
         return fig
 
 
-    def show_in_notebook(self, labels=None, predict_proba=True, text=None):
+    def show_in_notebook(self, labels=None, predict_proba=True, text=True):
         """Shows html explanation in ipython notebook.
+
            See as_html for parameters.
            This will throw an error if you don't have IPython installed"""
         from IPython.core.display import display, HTML
         display(HTML(self.as_html(labels, predict_proba, text)))
 
 
-    def as_html(self, labels=None, predict_proba=True, text=None):
+    def as_html(self, labels=None, predict_proba=True, text=True):
         """Returns the explanation as an html page.
+
         Args:
             labels: desired labels to show explanations for (as barcharts).
-                    If you ask for a label for which an explanation wasn't
-                    computed, will throw an exception. If None, will show
-                    explanations for all available labels.
+                If you ask for a label for which an explanation wasn't computed,
+                will throw an exception. If None, will show explanations for all
+                available labels.
             predict_proba: if true, add  barchart with prediction probabilities
-                           for the top classes.
-            text: raw text data. If present, include the text in the HTML page,
-                  with words in the explanation highlighted with the appropriate
-                  color. If more than one label is present, highlights the words
-                  according to the explanation of labels[0].
+                for the top classes.
+            text: If True, include the text in the HTML page, with words in the
+                explanation highlighted with the appropriate color. If more than
+                one label is present, highlights the words according to the
+                explanation of labels[0].
 
         Returns:
             code for an html page, including javascript includes.
@@ -129,13 +136,27 @@ class Explanation(object):
         <div id="mychart%s" style="float:left"></div>
         ''' % (random_id)
 
-        if text is not None:
-            text = cgi.escape(text).encode('ascii', 'xmlcharrefreplace')
-            for word, val in self.as_list(labels[0]):
+        if text:
+            text = self.indexed_string.raw_string().encode('ascii', 'xmlcharrefreplace')
+            # removing < > and & to display in html
+            text = re.sub(r'[<>&]', '|', text)
+            exp = self.as_list(labels[0], positions=True)
+            all_ocurrences = list(itertools.chain.from_iterable(
+                [itertools.product([x[0]], x[1], [x[2]]) for x in exp]))
+            sorted_ocurrences = sorted(all_ocurrences, key=lambda x: x[1])
+            add_after = '</span>'
+            added = 0
+            for word, position, val in sorted_ocurrences:
                 class_ = 'pos' if val > 0 else 'neg'
-                text = re.sub(r'(\W|^)(%s)(\W|$)' % word,
-                              r'\1<span class="%s">\2</span>\3' % class_,
-                              text)
+                add_before = '<span class="%s">' % class_
+                idx0 = position + added
+                idx1 = idx0 + len(word)
+                text = '%s%s%s%s%s' % (text[:idx0],
+                                       add_before,
+                                       text[idx0:idx1],
+                                       add_after,
+                                       text[idx1:])
+                added += len(add_before) + len(add_after)
             text = re.sub('\n', '<br />', text)
             out += ('<div id="mytext%s"><h3>Text with highlighted words</h3>'
                     '%s</div>' % (random_id, text))
--- lime/lime_text.py
+++ lime/lime_text.py
@@ -6,6 +6,107 @@ from . import explanation
 import numpy as np
 import scipy as sp
 import sklearn
+import re
+import itertools
+
+class ScikitClassifier(object):
+    """ Takes a classifier and vectorizer, implements predict_proba on raw text.
+    """
+    def __init__(self, classifier, vectorizer):
+        """Initializer.
+
+        Args:
+            classifier: trained sklearn classifier, must implement predict_proba
+            vectorizer: Count or Tfidf vectorizers, in sklearn.feature_extraction.text
+        """
+        self.classifier = classifier
+        self.vectorizer = vectorizer
+    def predict_proba(self, string_list):
+        """Transforms and predicts on an input string
+
+        Args:
+            string_list: list of raw text strings.
+
+        Returns:
+            same as classifier.predict_proba
+        """
+        return self.classifier.predict_proba(
+            self.vectorizer.transform(string_list))
+
+class IndexedString(object):
+    """String with various indexes."""
+    def __init__(self, raw_string, bow=True):
+        """Initializer.
+
+        Args:
+            raw_string: string with raw text in it
+            bow: if True, a word is the same everywhere in the text - i.e. we
+                 will index multiple ocurrences of the same word. If False,
+                 order matters, so that the same word will have different ids
+                 according to position.
+        """
+        self.raw = raw_string
+        self.as_list = re.split(r'(\W+)', self.raw)
+        self.as_np = np.array(self.as_list)
+        non_word = re.compile(r'\W+').match
+        self.string_start = np.hstack(([0], np.cumsum([len(x) for x in self.as_np[:-1]])))
+        vocab = {}
+        self.inverse_vocab = []
+        self.positions = []
+        self.bow = bow
+        non_vocab = set()
+        for i, word in enumerate(self.as_np):
+            if word in non_vocab:
+                continue
+            if non_word(word):
+                non_vocab.add(word)
+                continue
+            if bow:
+                if word not in vocab:
+                    vocab[word] = len(vocab)
+                    self.inverse_vocab.append(word)
+                    self.positions.append([])
+                idx_word = vocab[word]
+                self.positions[idx_word].append(i)
+            else:
+                self.inverse_vocab.append(word)
+                self.positions.append(i)
+        if not bow:
+            self.positions = np.array(self.positions)
+    def raw_string(self):
+        """Returns the original raw string"""
+        return self.raw
+    def num_words(self):
+        """Returns the number of tokens in the vocabulary for this document."""
+        return len(self.inverse_vocab)
+    def word(self, id_):
+        """Returns the word that corresponds to id_ (int)"""
+        return self.inverse_vocab[id_]
+    def string_position(self, id_):
+        """Returns a np array with indices to id_ (int) ocurrences"""
+        if self.bow:
+            return self.string_start[self.positions[id_]]
+        else:
+            return self.string_start[[self.positions[id_]]]
+    def inverse_removing(self, words_to_remove):
+        """Returns a string after removing the appropriate words.
+
+        Args:
+            words_to_remove: list of ids (ints) to remove
+
+        Returns:
+            original raw string with appropriate words removed.
+        """
+        mask = np.ones(self.as_np.shape[0], dtype='bool')
+        mask[self.__get_idxs(words_to_remove)] = False
+        return ''.join([self.as_list[v] for v in mask.nonzero()[0]])
+    def __get_idxs(self, words):
+        """Returns indexes to appropriate words."""
+        if self.bow:
+            return list(itertools.chain.from_iterable([self.positions[z] for z in words]))
+        else:
+            return self.positions[words]
+
 
 class LimeTextExplainer(object):
     """Explains text classifiers.
@@ -14,23 +115,27 @@ class LimeTextExplainer(object):
     def __init__(self,
                  kernel_width=25,
                  verbose=False,
-                 vocabulary=None,
                  class_names=None,
-                 feature_selection='auto'):
+                 feature_selection='auto',
+                 bow='bow'):
         """Init function.
 
         Args:
             kernel_width: kernel width for the exponential kernel
-            verbose: if true, print local prediction values from linera model
-            vocabulary: map from word to feature id. If present, explanations
-                        will be returned with words, instead of feature ids.
+            verbose: if true, print local prediction values from linear model
             class_names: list of class names, ordered according to whatever the
-                         classifier is using. If not present, class names will
-                         be '0', '1', ...
+                classifier is using. If not present, class names will be '0',
+                '1', ...
             feature_selection: feature selection method. can be
                 'forward_selection', 'lasso_path', 'none' or 'auto'.
                 See function 'explain_instance_with_data' in lime_base.py for
                 details on what each of the options does.
+            bow: if True (bag of words), will perturb input data by removing all
+                ocurrences of individual words.  Explanations will be in terms of
+                these words. Otherwise, will explain in terms of word-positions,
+                so that a word may be important the first time it appears and
+                uninportant the second. Only set to false if the classifier uses
+                word order in some way (bigrams, etc).
 
         """
         # exponential kernel
@@ -39,12 +144,9 @@ class LimeTextExplainer(object):
         self.class_names = class_names
         self.vocabulary = None
         self.feature_selection = feature_selection
-        if vocabulary:
-            terms = np.array(list(vocabulary.keys()))
-            indices = np.array(list(vocabulary.values()))
-            self.vocabulary = terms[np.argsort(indices)]
+        self.bow = bow
     def explain_instance(self,
-                         instance,
+                         text_instance,
                          classifier_fn,
                          labels=(1,),
                          top_labels=None,
@@ -56,17 +158,16 @@ class LimeTextExplainer(object):
         the instance (see __data_labels_distance_mapping). We then learn
         locally weighted linear models on this neighborhood data to explain each
         of the classes in an interpretable way (see lime_base.py).
+
         Args:
-            instance: instance to be explained. Assumed to be a
-                      sp.sparse.csr_matrix with one row.
-            classifier_fn: classifier prediction probability function, which takes a
-                        sparse matrix and outputs prediction probabilities.
-                        For scikit-learn classifiers, this is
-                        classifier.predict_proba.
+            text_instance: raw text string to be explained.
+            classifier_fn: classifier prediction probability function, which
+                takes a string and outputs prediction probabilities.  For
+                ScikitClassifiers , this is classifier.predict_proba.
             labels: iterable with labels to be explained.
             top_labels: if not None, ignore labels and produce explanations for
-                        the K labels with highest prediction probabilities,
-                        where K is this parameter.
+                the K labels with highest prediction probabilities, where K is
+                this parameter.
             num_features: maximum number of features present in explanation
             num_samples: size of the neighborhood to learn the linear model
 
@@ -74,73 +175,64 @@ class LimeTextExplainer(object):
             An Explanation object (see explanation.py) with the corresponding
             explanations.
         """
-        instance = sp.sparse.csr_matrix(instance)
-        data, yss, distances, mapping = self.__data_labels_distances_mapping(
-            instance, classifier_fn, num_samples)
+        indexed_string = IndexedString(text_instance, bow=self.bow)
+        data, yss, distances = self.__data_labels_distances(
+            indexed_string, classifier_fn, num_samples)
         if not self.class_names:
             self.class_names = [str(x) for x in range(yss[0].shape[0])]
-        ret_exp = explanation.Explanation(
-            vocabulary=self.vocabulary, class_names=self.class_names)
+        ret_exp = explanation.Explanation(indexed_string=indexed_string,
+                                          class_names=self.class_names)
         ret_exp.predict_proba = yss[0]
-        map_exp = lambda exp: [(mapping[x[0]], x[1]) for x in exp]
+        #map_exp = lambda exp: [(indexed_string.word(x[0]), x[1]) for x in exp]
         if top_labels:
             labels = np.argsort(yss[0])[-top_labels:]
             ret_exp.top_labels = list(labels)
             ret_exp.top_labels.reverse()
         for label in labels:
-            ret_exp.local_exp[label] = map_exp(
-                self.base.explain_instance_with_data(
-                    data, yss, distances, label, num_features,
-                    feature_selection=self.feature_selection))
+            ret_exp.local_exp[label] = self.base.explain_instance_with_data(
+                data, yss, distances, label, num_features,
+                feature_selection=self.feature_selection)
         return ret_exp
-    @staticmethod
-    def __data_labels_distances_mapping(instance, classifier_fn, num_samples):
+
+    @classmethod
+    def __data_labels_distances(cls,
+                                indexed_string,
+                                classifier_fn,
+                                num_samples):
         """Generates a neighborhood around a prediction.
 
-        Generates neighborhood data by randomly hiding features from
+        Generates neighborhood data by randomly removing words from
         the instance, and predicting with the classifier. Uses cosine distance
         to compute distances between original and perturbed instances.
         Args:
-            instance: instance to be explained. Assumed to be a
-                      sp.sparse.csr_matrix with one row.
+            indexed_string: document (IndexedString) to be explained,
             classifier_fn: classifier prediction probability function, which
-                           takes a sparse matrix and outputs prediction
-                           probabilities. For scikit-learn classifiers, this is
-                           classifier.predict_proba.
+                takes a string and outputs prediction probabilities. For
+                ScikitClassifier, this is classifier.predict_proba.
             num_samples: size of the neighborhood to learn the linear model
 
         Returns:
-            A tuple (data, labels, distances, mapping), where:
+            A tuple (data, labels, distances), where:
                 data: dense num_samples * K binary matrix, where K is the
-                      number of nonzero words in instance. The first row is
-                      the original instance, and thus a row of ones.
+                    number of tokens in indexed_string. The first row is the
+                    original instance, and thus a row of ones.
                 labels: num_samples * L matrix, where L is the number of target
-                        labels
+                    labels
                 distances: cosine distance between the original instance and
-                           each perturbed instance, times 100.
-                mapping: correspondance between the features in data and the
-                         features in the original problem. mapping[0] is the
-                         feature that corresponds to data[:, 0].
+                    each perturbed instance (computed in the binary 'data'
+                    matrix), times 100.
         """
         distance_fn = lambda x: sklearn.metrics.pairwise.cosine_distances(x[0], x)[0] * 100
-        features = instance.nonzero()[1]
-        vals = np.array(instance[instance.nonzero()])[0]
-        doc_size = len(sp.sparse.find(instance)[2])
+        doc_size = indexed_string.num_words()
         sample = np.random.randint(1, doc_size, num_samples - 1)
-        data = np.zeros((num_samples, len(features)))
-        inverse_data = np.zeros((num_samples, len(features)))
+        data = np.ones((num_samples, doc_size))
         data[0] = np.ones(doc_size)
-        inverse_data[0] = vals
-        features_range = range(len(features))
+        features_range = range(doc_size)
+        inverse_data = [indexed_string.raw_string()]
         for i, size in enumerate(sample, start=1):
-            active = np.random.choice(features_range, size, replace=False)
-            data[i, active] = 1
-            inverse_data[i, active] = vals[active]
-        sparse_inverse = sp.sparse.lil_matrix(
-            (inverse_data.shape[0], instance.shape[1]))
-        sparse_inverse[:, features] = inverse_data
-        sparse_inverse = sp.sparse.csr_matrix(sparse_inverse)
-        mapping = features
-        labels = classifier_fn(sparse_inverse)
-        distances = distance_fn(sparse_inverse)
-        return data, labels, distances, mapping
+            inactive = np.random.choice(features_range, size, replace=False)
+            data[i, inactive] = 0
+            inverse_data.append(indexed_string.inverse_removing(inactive))
+        labels = classifier_fn(inverse_data)
+        distances = distance_fn(sp.sparse.csr_matrix(data))
+        return data, labels, distances
