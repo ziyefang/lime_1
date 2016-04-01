minor fix in split, fixed data weighting in lasso method.

lime/lime_base.py
@@ -71,10 +71,8 @@ class LimeBase(object):
                                      reverse=True)
             return np.array([x[0] for x in feature_weights[:num_features]])
         elif method == 'lasso_path':
-            weighted_data = data * weights[:, np.newaxis]
-            mean = np.mean(labels)
-            shifted_labels = labels - mean
-            weighted_labels = shifted_labels * weights
+            weighted_data = (data - np.average(data, axis=0, weights=weights)) * np.sqrt(weights[:, np.newaxis])
+            weighted_labels = (labels - np.average(labels, weights=weights)) * np.sqrt(weights)
             used_features = range(weighted_data.shape[1])
             nonzero = range(weighted_data.shape[1])
             _, coefs = self.generate_lars_path(weighted_data,

lime/lime_text.py
@@ -47,9 +47,9 @@ class IndexedString(object):
                  according to position.
         """
         self.raw = raw_string
-        self.as_list = re.split(r'(%s)' % split_expression, self.raw)
+        self.as_list = re.split(r'(%s)|$' % split_expression, self.raw)
         self.as_np = np.array(self.as_list)
-        non_word = re.compile(r'(%s)' % split_expression).match
+        non_word = re.compile(r'(%s)|$' % split_expression).match
         self.string_start = np.hstack(([0], np.cumsum([len(x) for x in self.as_np[:-1]])))
         vocab = {}
         self.inverse_vocab = []