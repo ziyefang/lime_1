3f72f42902bb24b3365c82018c4f141d256bed7d
Dev Gupta <dev@predikto.com>
2016-09-06 12:03:11 -0400

    Synthetic test and better asserts
    Added a synthetic test for classification along with better classification asserts (making sure the expected features  are seen in the explaination)

--- lime/tests/test_lime_text.py
+++ lime/tests/test_lime_text.py
@@ -14,14 +14,10 @@ class TestLimeText(unittest.TestCase):
 
     def test_lime_text_explainer_good_regressor(self):
         from sklearn.datasets import fetch_20newsgroups
-        newsgroups_train = fetch_20newsgroups(subset='train')
-        newsgroups_test = fetch_20newsgroups(subset='test')
-        # making class names shorter
-        class_names = [x.split('.')[-1] if 'misc' not in x
-                       else '.'.join(x.split('.')[-2:])
-                       for x in newsgroups_train.target_names]
-        class_names[3] = 'pc.hardware'
-        class_names[4] = 'mac.hardware'
+        categories = ['alt.atheism', 'soc.religion.christian']
+        newsgroups_train = fetch_20newsgroups(subset='train', categories=categories)
+        newsgroups_test = fetch_20newsgroups(subset='test', categories=categories)
+        class_names = ['atheism', 'christian']
         vectorizer = TfidfVectorizer(lowercase=False)
         train_vectors = vectorizer.fit_transform(newsgroups_train.data)
         test_vectors = vectorizer.transform(newsgroups_test.data)
@@ -31,13 +27,10 @@ class TestLimeText(unittest.TestCase):
         f1_score(newsgroups_test.target, pred, average='weighted')
         c = make_pipeline(vectorizer, nb)
         explainer = LimeTextExplainer(class_names=class_names)
-        idx = 1340
-        exp = explainer.explain_instance(newsgroups_test.data[idx],
-                                         c.predict_proba,
-                                         num_features=6,
-                                         labels=[0, 17],
-                                         model_regressor=LinearRegression())
+        idx = 83
+        exp = explainer.explain_instance(newsgroups_test.data[idx], c.predict_proba, num_features=6)
         self.assertIsNotNone(exp)
+        self.assertEquals(6, len(exp.as_list()))
 
     def test_lime_text_explainer_bad_regressor(self):
         from sklearn.datasets import fetch_20newsgroups
