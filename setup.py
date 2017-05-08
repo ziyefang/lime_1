ea67c373b7136e9334fa6f0fcd4e7ac7ccf9911d
aikramer2 <aikramer2@gmail.com>
2017-05-08 10:02:38 -0700

    bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='lime',
-      version='0.1.1.20',
+      version='0.1.1.21',
       description='Local Interpretable Model-Agnostic Explanations for machine learning classifiers',
       url='http://github.com/marcotcr/lime',
       author='Marco Tulio Ribeiro',
