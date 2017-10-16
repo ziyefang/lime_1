03e58b256591fbd3dca5346a42ee48d25e05c43e
Pajot, Florent <florent.pajot@continental-corporation.com>
2017-10-16 15:03:08 +0000

    corrected flake8 requirements

--- lime/tests/test_scikit_image.py
+++ lime/tests/test_scikit_image.py
@@ -9,116 +9,119 @@ import numpy as np
 
 class TestBaseWrapper(unittest.TestCase):
 
-	def test_base_wrapper(self):
+    def test_base_wrapper(self):
 
-		obj_with_params = BaseWrapper(a=10, b='message')
-		obj_without_params = BaseWrapper()
+        obj_with_params = BaseWrapper(a=10, b='message')
+        obj_without_params = BaseWrapper()
 
-		def foo_fn():
-			return 'bar'
+        def foo_fn():
+            return 'bar'
 
-		obj_with_fn = BaseWrapper(foo_fn)
-		self.assertEqual(obj_with_params.target_params, {'a': 10, 'b': 'message'})
-		self.assertEqual(obj_without_params.target_params, {})
-		self.assertEqual(obj_with_fn.target_fn(), 'bar')
+        obj_with_fn = BaseWrapper(foo_fn)
+        self.assertEqual(obj_with_params.target_params, {'a': 10, 'b': 'message'})
+        self.assertEqual(obj_without_params.target_params, {})
+        self.assertEqual(obj_with_fn.target_fn(), 'bar')
 
-	def test__check_params(self):
+    def test__check_params(self):
 
-		def bar_fn(a):
-			return str(a)
+        def bar_fn(a):
+            return str(a)
 
-		class Pipo():
+        class Pipo():
 
-			def __init__(self):
-				self.name = 'pipo'
+            def __init__(self):
+                self.name = 'pipo'
 
-			def __call__(self, message):
-				return message
+            def __call__(self, message):
+                return message
 
-		pipo = Pipo()
-		obj_with_valid_fn = BaseWrapper(bar_fn, a=10, b='message')
-		obj_with_valid_callable_fn = BaseWrapper(pipo, c=10, d='message')
-		obj_with_invalid_fn = BaseWrapper([1, 2, 3], fn_name='invalid')
+        pipo = Pipo()
+        obj_with_valid_fn = BaseWrapper(bar_fn, a=10, b='message')
+        obj_with_valid_callable_fn = BaseWrapper(pipo, c=10, d='message')
+        obj_with_invalid_fn = BaseWrapper([1, 2, 3], fn_name='invalid')
 
-		# target_fn is not a callable or function/method
-		with self.assertRaises(AttributeError):
-			obj_with_invalid_fn._check_params('fn_name')
+        # target_fn is not a callable or function/method
+        with self.assertRaises(AttributeError):
+            obj_with_invalid_fn._check_params('fn_name')
 
-		# parameters is not in target_fn args
-		with self.assertRaises(ValueError):
-			obj_with_valid_fn._check_params(['c'])
-			obj_with_valid_callable_fn._check_params(['e'])
+        # parameters is not in target_fn args
+        with self.assertRaises(ValueError):
+            obj_with_valid_fn._check_params(['c'])
+            obj_with_valid_callable_fn._check_params(['e'])
 
-		# params is in target_fn args
-		try:
-			obj_with_valid_fn._check_params(['a'])
-			obj_with_valid_callable_fn._check_params(['message'])
-		except Exception:
-			self.fail("_check_params() raised an unexpected exception")
+        # params is in target_fn args
+        try:
+            obj_with_valid_fn._check_params(['a'])
+            obj_with_valid_callable_fn._check_params(['message'])
+        except Exception:
+            self.fail("_check_params() raised an unexpected exception")
 
-		# params is not a dict or list
-		with self.assertRaises(TypeError):
-			obj_with_valid_fn._check_params(None)
-		with self.assertRaises(TypeError):
-			obj_with_valid_fn._check_params('param_name')
+        # params is not a dict or list
+        with self.assertRaises(TypeError):
+            obj_with_valid_fn._check_params(None)
+        with self.assertRaises(TypeError):
+            obj_with_valid_fn._check_params('param_name')
 
-	def test_set_params(self):
+    def test_set_params(self):
 
-		class Pipo():
+        class Pipo():
 
-			def __init__(self):
-				self.name = 'pipo'
+            def __init__(self):
+                self.name = 'pipo'
 
-			def __call__(self, message):
-				return message
-		pipo = Pipo()
-		obj = BaseWrapper(pipo)
+            def __call__(self, message):
+                return message
+        pipo = Pipo()
+        obj = BaseWrapper(pipo)
 
-		# argument is set accordingly
-		obj.set_params(message='OK')
-		self.assertEqual(obj.target_params, {'message': 'OK'})
-		self.assertEqual(obj.target_fn(**obj.target_params), 'OK')
+        # argument is set accordingly
+        obj.set_params(message='OK')
+        self.assertEqual(obj.target_params, {'message': 'OK'})
+        self.assertEqual(obj.target_fn(**obj.target_params), 'OK')
 
-		# invalid argument is passed
-		try:
-			obj = BaseWrapper(Pipo())
-			obj.set_params(invalid='KO')
-		except:
-			self.assertEqual(obj.target_params, {})
+        # invalid argument is passed
+        try:
+            obj = BaseWrapper(Pipo())
+            obj.set_params(invalid='KO')
+        except:
+            self.assertEqual(obj.target_params, {})
 
-	def test_filter_params(self):
-        
-		# right arguments are kept and wrong dismmissed
-		def baz_fn(a, b, c=True):
-			if c:
-				return a + b
-			else:
-				return a
-		obj_ = BaseWrapper(baz_fn, a=10, b=100, d=1000)
-		self.assertEqual(obj_.filter_params(baz_fn), {'a': 10, 'b': 100})
+    def test_filter_params(self):
 
-		# target_params is overriden using 'override' argument
-		self.assertEqual(obj_.filter_params(baz_fn, override={'c': False}), {'a': 10, 'b': 100, 'c': False})
+        # right arguments are kept and wrong dismmissed
+        def baz_fn(a, b, c=True):
+            if c:
+                return a + b
+            else:
+                return a
+        obj_ = BaseWrapper(baz_fn, a=10, b=100, d=1000)
+        self.assertEqual(obj_.filter_params(baz_fn), {'a': 10, 'b': 100})
+
+        # target_params is overriden using 'override' argument
+        self.assertEqual(obj_.filter_params(baz_fn, override={'c': False}),
+                         {'a': 10, 'b': 100, 'c': False})
 
 
 class TestSegmentationAlgorithm(unittest.TestCase):
 
-	def test_instanciate_segmentation_algorithm(self):
-		img = img_as_float(astronaut()[::2, ::2])
-        
-		# wrapped functions provide the same result
-		fn = SegmentationAlgorithm('quickshift', kernel_size=3, max_dist=6, ratio=0.5, random_seed=133)
-		fn_result = fn(img)
-		original_result = quickshift(img, kernel_size=3, max_dist=6, ratio=0.5, random_seed=133)
+    def test_instanciate_segmentation_algorithm(self):
+        img = img_as_float(astronaut()[::2, ::2])
 
-		# same segments
-		self.assertTrue(np.array_equal(fn_result, original_result))
+        # wrapped functions provide the same result
+        fn = SegmentationAlgorithm('quickshift', kernel_size=3, max_dist=6,
+                                   ratio=0.5, random_seed=133)
+        fn_result = fn(img)
+        original_result = quickshift(img, kernel_size=3, max_dist=6, ratio=0.5,
+                                     random_seed=133)
 
-	def test_instanciate_slic(self):
-		pass
+        # same segments
+        self.assertTrue(np.array_equal(fn_result, original_result))
 
-	def test_instanciate_felzenszwalb(self):
-		pass
+    def test_instanciate_slic(self):
+        pass
+
+    def test_instanciate_felzenszwalb(self):
+        pass
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
