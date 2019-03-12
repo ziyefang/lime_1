41d4deb89a66b537648ba2b555786fb0abf87943
Marco Tulio Ribeiro <marcotcr@gmail.com>
2019-03-12 13:13:11 -0700

    version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='lime',
-      version='0.1.1.32',
+      version='0.1.1.33',
       description='Local Interpretable Model-Agnostic Explanations for machine learning classifiers',
       url='http://github.com/marcotcr/lime',
       author='Marco Tulio Ribeiro',
